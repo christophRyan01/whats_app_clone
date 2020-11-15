353a86ac3def2279b515816d7443672b413c3be7
christopherRyan01 <chrisg450@gmail.com>
2020-11-14 17:24:46 -0800

    modifications to Contacts side bar and Contact list

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,12 +2,19 @@ import React from 'react'
 import useLocalStorage from '../hooks/useLocalStorage';
 import Login from './Login';
 import Dashboard from './Dashboard';
+import { ContactsProvider } from '../contexts/ContactsProvider';
 
 export default function App() {
   const [id, setId] = useLocalStorage('id');
 
+  const dashboard = (
+    <ContactsProvider>
+      <Dashboard id={id} />
+    </ContactsProvider>
+  )
+
   return (
-      id ? <Dashboard id={id} /> : <Login onIdSubmit = {setId}/>
+      id ? dashboard : <Login onIdSubmit = {setId}/>
    
   );
 }
